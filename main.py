b9240d9578b2e825da0a8d66a8a0bdf223b58b32
Ismail Malik <vesmalick@gmail.com>
2026-05-27 13:23:35 +0300

    Implemented the create_todo endpoint

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("Hello World!")
+from pydantic import BaseModel
+from fastapi import FastAPI
+
+app = FastAPI()
+
+class TodoCreate(BaseModel):
+    title: str
+    description: str | None = None
+    is_completed: bool = False
+
+todos = []
+next_id = 1
+
+@app.post("/todos", status_code=201)
+async def create_todo(todo: TodoCreate):
+    global next_id
+    id = next_id
+    next_id = next_id + 1
+
+    todo = {
+        "id": id,
+        **todo.model_dump()
+    }
+
+    todos.append(todo)
+
+    return todo
+
+
