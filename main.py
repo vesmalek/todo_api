7c6f877aa4ee744b297e05c336b89d9c5efb4d1c
Ismail Malik <vesmalick@gmail.com>
2026-05-27 13:36:09 +0300

    Started creating the retrieve all todos

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ class TodoCreate(BaseModel):
 todos = []
 next_id = 1
 
+@app.get("/todos")
+async def get_todos(skip: int = 0, limit: int = 2, is_checked: bool | None = None):
+    return todos[skip: skip + limit]
+
 @app.post("/todos", status_code=201)
 async def create_todo(todo: TodoCreate):
     global next_id
