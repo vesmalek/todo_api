c877b6e179b0f533a6bbb33484b0ff50bb8e298b
Ismail Malik <vesmalick@gmail.com>
2026-05-27 15:11:57 +0300

    Implemented update a todo feature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
@@ -8,8 +8,21 @@ class TodoCreate(BaseModel):
     description: str | None = None
     is_completed: bool = False
 
+class TodoUpdate(BaseModel):
+    title: str
+    description: str | None = None
+    is_completed: bool = False
+
 todos = []
 next_id = 1
+
+# Helper function for finding todo:
+def find_todo(todo_id: int) -> dict:
+    for todo in todos:
+        if todo["id"] == todo_id:
+            return todo
+        
+    raise HTTPException(status_code=404, detail="TODO not found!")
 
 @app.get("/todos")
 async def get_todos(skip: int = 0, limit: int = 4, is_checked: bool | None = None):
@@ -17,6 +30,14 @@ async def get_todos(skip: int = 0, limit: int = 4, is_checked: bool | None = Non
         return [todo for todo in todos if todo["is_completed"] == is_checked]
     
     return todos[skip: skip + limit]
+
+@app.get("/todos/{todo_id}")
+async def get_todo(todo_id: int):
+    
+    todo = find_todo(todo_id)
+    
+    if todo:
+        return todo
 
 @app.post("/todos", status_code=201)
 async def create_todo(todo: TodoCreate):
@@ -32,5 +53,16 @@ async def create_todo(todo: TodoCreate):
     todos.append(todo)
 
     return todo
+
+@app.put("/todos/{todo_id}")
+async def update_todo(todo_id: int, todo: TodoUpdate):
+    result = find_todo(todo_id)
+
+    if result:
+        result["title"] = todo.title
+        result["description"] = todo.description
+        result["is_completed"] = todo.is_completed
+
+    return result
 
 
